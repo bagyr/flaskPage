4344c34b105ea57b73cc1f3858b05e488c7ca09f
= <=>
2011-07-27 11:36:49 +0000

    Now it is actually servies something

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+import markovChainGenerator as mark
 
+chain = mark.LetterChain()
+chain.fromJson('/root/src/page/markovChainGenerator/kapitan.json')
+generator = mark.MarkovGenerator(chain, ' ')
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world(name = 'pasha'):
+def hello_world(name = 'Kill Me.'):
+    name = generator.makeText(20)
     return render_template('page.html', name=name)
 
 if __name__ == '__main__':
-    app.debug = True
+    app.debug = False 
     app.run(host='0.0.0.0')
 
